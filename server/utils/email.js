f2764c56c6784b4cf7265bd5874e9ae93f6dcf99
Prakhar Verma <prakharv824@gmail.com>
2026-09-24 23:37:41 +0530

    Optimize Nodemailer with connection pooling for faster email delivery

--- server/utils/email.js
+++ server/utils/email.js
@@ -3,11 +3,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service :'gmail',
-    auth:{
-        user : process.env.EMAIL_USER,
-        pass : process.env.EMAIL_PASS
-    }
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    },
+    pool: true,
+    maxConnections: 3,
+    maxMessages: 50
 });
 
 const sendBookingEmail = async (userEmail, userName, eventTitle) => {
