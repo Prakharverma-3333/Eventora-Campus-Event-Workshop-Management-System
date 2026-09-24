8b307963235b6fd419db2a29bb8fb0f2769ce971
Prakhar Verma <prakharv824@gmail.com>
2026-09-24 23:42:42 +0530

    Revert email transporter configuration to original

--- server/utils/email.js
+++ server/utils/email.js
@@ -3,14 +3,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    },
-    pool: true,
-    maxConnections: 3,
-    maxMessages: 50
+    service :'gmail',
+    auth:{
+        user : process.env.EMAIL_USER,
+        pass : process.env.EMAIL_PASS
+    }
 });
 
 const sendBookingEmail = async (userEmail, userName, eventTitle) => {
